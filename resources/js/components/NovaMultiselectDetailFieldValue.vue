662a2528c8aa52b43e8810c266066c239d9679b9
Ramón Muñiz <ramon@nitsnets.com>
2023-01-24 13:38:51 +0100

    fix error in show simple information

--- resources/js/components/NovaMultiselectDetailFieldValue.vue
+++ resources/js/components/NovaMultiselectDetailFieldValue.vue
@@ -2,7 +2,7 @@
   <div class="relative rounded-lg bg-white shadow border border-60" v-if="values">
     <div class="overflow-hidden rounded-b-lg rounded-t-lg">
       <div class="border-b border-50 cursor-text font-mono text-sm py-2 px-4 p-3 pb-2" v-for="(item, i) of values" :key="i">
-          <div v-if="item.label" class="block">
+          <div v-if="item.label && item.label.title" class="block">
             <div class="inline-block w-16 pr-2">
               <img v-viewer v-if="item.label.img" :src="item.label.img" :alt="item.label.code">
             </div>
@@ -12,9 +12,12 @@
               <span class="whitespace-no-wrap mt-1 text-gray-500 text-sm">{{ item.label.code }}</span>
             </div>
           </div>
+        <span v-else-if="item.label">
+            {{ item.label }}
+          </span>
         <span v-else>
             {{ item }}
-          </span>
+        </span>
       </div>
     </div>
   </div>
